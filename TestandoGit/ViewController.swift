f969c57288445559da0f40dcbadbce53a5f55cdf
alnp <alessandrapereira@outlook.com>
2022-07-04 17:04:07 -0300

    Adiciona projeto do xcode e realiza primeiro commit

---
+++ TestandoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestandoGit
+//
+//  Created by Alessandra Pereira on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
